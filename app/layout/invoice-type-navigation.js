c987b8899daf879b5dd208cafcdfebc2f4caeca7
Wojciech <wojciech.dobosz1@gmail.com>
2024-06-10 10:14:28 +0100

    Added active links

--- app/layout/invoice-type-navigation.js
+++ app/layout/invoice-type-navigation.js
@@ -1,16 +1,20 @@
+import { usePathname } from 'next/navigation'
 import Link from "next/link";
 
 export default function InvoiceTypeNavigation() {
+    const pathname = usePathname();
 
     const linkClassName = "text-gray-600 ms-3 font-light";
+    const active = "text-gray-600 ms-3 font-light border-b border-gray-300";
+    
     return(
         <>
         <div className="w-full flex justify-between bg-white px-5 lg:px-12 py-4 border-t  shadow-lg">
         <nav className="flex  md:ps-20 ms-2">
-                <Link href="/invoices" className="text-gray-600 font-light">All Invoices</Link>
-                <Link href="/invoices/pending" className={linkClassName}>Pending</Link>
-                <Link href="/invoices/excluded" className={linkClassName}>Excluded</Link>
-                <Link href="/invoices/paid" className={linkClassName}>Paid</Link>
+                <Link href="/invoices" className={`link ${pathname === '/invoices' ? `${active}` : `${linkClassName}`}`}>All Invoices</Link>
+                <Link href="/invoices/pending" className={`link ${pathname === '/invoices/pending' ? `${active}` : `${linkClassName}`}`}>Pending</Link>
+                <Link href="/invoices/excluded" className={`link ${pathname === '/invoices/excluded' ? `${active}` : `${linkClassName}`}`}>Excluded</Link>
+                <Link href="/invoices/paid" className={`link ${pathname === '/invoices/paid' ? `${active}` : `${linkClassName}`}`}>Paid</Link>
             </nav>
         </div>
         </>
