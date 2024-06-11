87236a5b282fe00f377efb7bb7b619c8cb724d01
Wojciech <wojciech.dobosz1@gmail.com>
2024-06-11 09:52:46 +0100

    Padding adjustment

--- app/components/dashbord-cards.js
+++ app/components/dashbord-cards.js
@@ -3,7 +3,7 @@ import Tile from "./UI/tile";
 export default function DashboardCards() {
   return (
     <>
-      <section className="flex flex-wrap justify-between mt-10">
+      <section className="flex flex-wrap justify-between mt-6">
         <Tile tileTitle="Invoices Number" tileValue="33" />
         <Tile tileTitle="Pending Credit" tileValue="£470,000" />
         <Tile tileTitle="Paid Amount" tileValue="£0,00" />
